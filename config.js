cad739c431b76574993ba7eb20c74d3f79e64ea5
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2018-08-20 20:46:58 -0400

    modified for aws

---
+++ config.js
@@ -0,0 +1,11 @@
+
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jqex3BIk2',
+        userPoolClientId: '',
+        region: '4aeh1ted99l4ng0htkdbg90r4i'
+    },
+    api: {
+        invokeUrl: 'http://www.liveaugusta.com.s3-website-us-east-1.amazonaws.com/'
+    }
+};
